35ab802c607db8a538bb3d4c03400a40003f42be
Prakhar <prakhar6601@gmail.com>
2024-03-31 19:35:17 +0530

    Adding PullRequest

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -2,6 +2,8 @@ import { getServerSession } from "next-auth";
 import { redirect } from 'next/navigation'
 import { authOptions } from "./lib/auth";
 
+
+
 export default async function Page() {
   const session = await getServerSession(authOptions);
   if (session?.user) {
