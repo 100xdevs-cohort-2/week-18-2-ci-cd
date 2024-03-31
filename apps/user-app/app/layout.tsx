665e94b6d4d1e6cdfb1cdf3db5911cbe290f2913
markande98 <gauravbpit1998@gmail.com>
2024-03-31 19:31:47 +0530

    updated app title

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -7,7 +7,7 @@ import { AppbarClient } from "../components/AppbarClient";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Wallet",
+  title: "Paytm Wallet",
   description: "Simple wallet app",
 };
 
