03c62bdb4be90b7d45dc1b8bfd5e72e28efe3de0
Rimaru06 <shivraodanger04@gmail.com>
2024-04-03 15:43:18 +0530

    spaces

--- apps/user-app/app/lib/auth.ts
+++ apps/user-app/app/lib/auth.ts
@@ -2,9 +2,6 @@ import db from "@repo/db/client";
 import CredentialsProvider from "next-auth/providers/credentials"
 import bcrypt from "bcrypt";
 
-
-
-
 export const authOptions = {
     providers: [
       CredentialsProvider({
