aaa1b983f59585b8b17969497da3d888843c06b6
Rimaru06 <shivraodanger04@gmail.com>
2024-04-03 15:39:19 +0530

    helo

--- apps/user-app/app/lib/auth.ts
+++ apps/user-app/app/lib/auth.ts
@@ -2,6 +2,9 @@ import db from "@repo/db/client";
 import CredentialsProvider from "next-auth/providers/credentials"
 import bcrypt from "bcrypt";
 
+
+
+
 export const authOptions = {
     providers: [
       CredentialsProvider({
