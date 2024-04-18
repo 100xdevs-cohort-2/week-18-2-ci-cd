8f52910221a410305a2ccd693de5caf6555f38e8
Akshat <iamakku0.0.0.1@gmail.com>
2024-04-19 01:41:23 +0530

    balance update for new user issue resolved

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -2,54 +2,108 @@ import express from "express";
 import db from "@repo/db/client";
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.post("/hdfcWebhook", async (req, res) => {
-    //TODO: Add zod validation here?
-    //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
-    const paymentInformation: {
-        token: string;
-        userId: string;
-        amount: string
-    } = {
-        token: req.body.token,
-        userId: req.body.user_identifier,
-        amount: req.body.amount
-    };
 
-    try {
-        await db.$transaction([
-            db.balance.updateMany({
-                where: {
-                    userId: Number(paymentInformation.userId)
-                },
-                data: {
-                    amount: {
-                        // You can also get this from your DB
-                        increment: Number(paymentInformation.amount)
-                    }
-                }
-            }),
-            db.onRampTransaction.updateMany({
-                where: {
-                    token: paymentInformation.token
-                }, 
-                data: {
-                    status: "Success",
-                }
-            })
-        ]);
+app.get("/",(req,res)=>{
+    console.log(`app running on port ${3001}`)
+    return res.json({
+        "mess":"server working fine"
+    })
+})
 
-        res.json({
-            message: "Captured"
+interface paymentInformationType {
+    token : string,
+    userId: number,
+    amount : number
+}
+
+//bank will hit this endpoint to update the user balance wallet
+app.post("/hdfcWebhook",async(req,res)=>{
+
+    try{
+
+        const paymentInformation : paymentInformationType = {
+            token : req.body.token,
+            userId : req.body.userId,
+            amount : req.body.amount
+        }
+
+        const userData = await db.user.findUnique({
+            where:{
+                id:paymentInformation.userId
+            },
+            include:{
+                Balance:true ,
+                OnRampTransaction:true
+            }
         })
-    } catch(e) {
-        console.error(e);
-        res.status(411).json({
-            message: "Error while processing webhook"
+
+if(userData?.Balance.length === 0){
+        //using transactions to do 2 db query in single request so that if 1 fail 2nd also fail's 
+        await db.$transaction([
+
+            db.balance.create({
+               data:{
+                userId:paymentInformation.userId,
+                   amount:paymentInformation.amount,
+                   locked:0
+               }
+           }),
+       
+           db.onRampTransaction.updateMany({
+               where:{
+                   token: String(paymentInformation.token)
+               },
+               data:{
+                   status:"Success"
+               }
+           })
+       ])
+
+     return  res.status(200).json({
+           mess:"req captured and updated"
+       })
+}
+else{
+        //using transactions to do 2 db query in single request so that if 1 fail 2nd also fail's 
+        await db.$transaction([
+            // updating balance not creating for the one having balance none
+            //the prisma will throw error saying no records found
+            db.balance.updateMany({
+               where:{
+                   userId: Number(paymentInformation.userId)
+               },
+               data:{
+                   amount:{
+                       increment: Number(paymentInformation.amount)
+                   }
+               }
+           }),
+       
+           db.onRampTransaction.updateMany({
+               where:{
+                   token: String(paymentInformation.token)
+               },
+               data:{
+                   status:"Success"
+               }
+           })
+       ])
+
+     return  res.status(200).json({
+           mess:"req captured and updated"
+       })
+}  }
+    catch(e){
+        console.log(e);
+      return  res.status(411).json({
+            mess:"internal server error",
         })
     }
-
 })
 
 app.listen(3003);
+
+
+
