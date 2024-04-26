4a2de4de9af828ab512519a6c6d9cdf76342280c
Zaki abdullahi <qable2121@gmail.com>
2024-04-26 08:23:39 +0300

    added console.log 2 apps/user-app/app/page.tsx

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -2,7 +2,9 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
 import { authOptions } from "./lib/auth";
+
 console.log("test");
+console.log("test2");
 
 export default async function Page() {
   const session = await getServerSession(authOptions);
