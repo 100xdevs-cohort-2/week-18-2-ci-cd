93184434c9eab8716e93f10b81c64d2e74489b45
Zaki abdullahi <qable2121@gmail.com>
2024-04-26 08:05:34 +0300

    added space apps/user-app/app/page.tsx

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,12 +1,12 @@
 import { getServerSession } from "next-auth";
-import { redirect } from 'next/navigation'
+import { redirect } from "next/navigation";
 import { authOptions } from "./lib/auth";
 
 export default async function Page() {
   const session = await getServerSession(authOptions);
   if (session?.user) {
-    redirect('/dashboard')
+    redirect("/dashboard");
   } else {
-    redirect('/api/auth/signin')
+    redirect("/api/auth/signin");
   }
 }
