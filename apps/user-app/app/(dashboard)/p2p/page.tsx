0bc84053b480bf3e1143a860feba6258be41656b
unknown <jyonaidu99@gmail.com>
2024-05-16 00:48:24 -0400

    changed p2p

--- apps/user-app/app/(dashboard)/p2p/page.tsx
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -2,6 +2,9 @@ import { SendCard } from "../../../components/SendCard";
 
 export default function() {
     return <div className="w-full"> 
+    <div >
+        Hello from p2p
+    </div>
         <SendCard />
     </div>
 }
