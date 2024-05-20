fd52689b997ebad4d02ec1f58cb72a54fa2c1981
Zafeeruddin <mohammed.xafeer@gmail.com>
2024-05-20 10:50:02 +0530

    afs

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -6,7 +6,7 @@ app.use(express.json())
 
 app.post("/hdfcWebhook", async (req, res) => {
     //TODO: Add zod validation here?
-    //TODO: HDFC bank should ideally send us a secret so we know this is sent by them but why?
+    //TODO: HDFC bank should ideally send us a secret so we know this is sent by them 
     const paymentInformation: {
         token: string;
         userId: string;
