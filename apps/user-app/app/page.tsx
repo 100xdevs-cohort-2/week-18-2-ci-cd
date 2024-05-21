0b4906fcac3a913eac725d728a0fb69657f7780b
ShashankDahake7 <shashankdahake578@gmail.com>
2024-05-21 19:02:43 +0530

    Commit

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -6,7 +6,8 @@ export default async function Page() {
   const session = await getServerSession(authOptions);
   if (session?.user) {
     redirect('/dashboard')
-  } else {
+  } 
+  else {
     redirect('/api/auth/signin')
   }
 }
