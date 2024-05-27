d6f606377ef4c0db77168c450255688e7e12541c
Satvik <2022uee0148@iitjammu.ac.in>
2024-05-27 15:30:20 +0530

    trial PR

--- apps/user-app/components/AddMoneyCard.tsx
+++ apps/user-app/components/AddMoneyCard.tsx
@@ -2,10 +2,9 @@
 import { Button } from "@repo/ui/button";
 import { Card } from "@repo/ui/card";
 import { Center } from "@repo/ui/center";
-
-
 import { Select } from "@repo/ui/select";
 import { useState } from "react";
+
 import { TextInput } from "@repo/ui/textinput";
 import { createOnRampTransaction } from "../app/lib/actions/createOnRamptxn";
 
