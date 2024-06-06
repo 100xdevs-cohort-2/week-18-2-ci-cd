85dd075f107c826b0ba225b277e84f552fd905ef
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-06-07 00:36:21 +0530

    added p2p transaction component

--- apps/user-app/app/(dashboard)/p2p/page.tsx
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -1,7 +1,39 @@
+import { getServerSession } from "next-auth";
 import { SendCard } from "../../../components/SendCard";
+import prisma from "@repo/db/client";
+import { authOptions } from "../../lib/auth";
+import { P2PTransactions } from "../../../components/P2PTransaction";
 
-export default function() {
-    return <div className="w-full"> 
-        <SendCard />
-    </div>
+
+async function getP2PTransactions() {
+  const session = await getServerSession(authOptions);
+  const txns = await prisma.p2pTransfer.findMany({
+      where: {
+          fromUserId: Number(session?.user?.id)
+      }
+  });
+  return txns.map((t: { timestamp: Date; amount: number; }) => ({
+      time: t.timestamp,
+      amount: t.amount
+  }))
+}
+
+export default async function Home() {
+  const transactions=await getP2PTransactions();
+
+  return (
+    <>
+      <div className="relative items-center min-h-screen">
+        P2P
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-4">
+        <div className="mt-10 w-full relative left-1/2  border border-slate-400">
+          <SendCard />
+        </div>
+        <div className="bg-white mt-10 w-full relative left-1/2  border border-slate-400">
+          <P2PTransactions transactions={transactions}/>
+        </div>
+      </div>
+        </div>
+    </>
+  );
 }
