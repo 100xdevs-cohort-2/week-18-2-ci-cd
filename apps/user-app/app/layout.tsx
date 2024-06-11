399122ba98e17662c4d9207c6b28ce60d957780f
Adarsh Rawat <00adarshrawat00@gmail.com>
2024-06-11 18:33:35 +0530

    third commit

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -1,8 +1,8 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { Providers } from "../provider";
-import { AppbarClient } from "../components/AppbarClient";
+// import "./globals.css";
+// import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+// import { Providers } from "../provider";
+// import { AppbarClient } from "../components/AppbarClient";
 
 const inter = Inter({ subsets: ["latin"] });
 
