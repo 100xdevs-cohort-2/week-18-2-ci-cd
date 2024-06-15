5bd3976a74f8da3387548c4d6fc215a0e58c42c8
Vishnu21710 <vishnu.mupparathy21@gmail.com>
2024-06-15 10:23:23 +0530

    testing CI --- 1

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -7,6 +7,11 @@ import { authOptions } from "./lib/auth";
 
 
 export default async function Page() {
+
+
+
+
+  
   const session = await getServerSession(authOptions);
   if (session?.user) {
     redirect('/dashboard')
